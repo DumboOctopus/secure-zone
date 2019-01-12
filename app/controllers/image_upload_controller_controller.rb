1b4f7e9b6bf90e43dbdc343bf87a19d825ddb0f7
NEAL <neilp011@gmail.com>
2019-01-12 10:38:17 +0000

    Image Uploader

--- app/controllers/image_upload_controller_controller.rb
+++ app/controllers/image_upload_controller_controller.rb
@@ -1,2 +1,12 @@
 class ImageUploadControllerController < ApplicationController
+    def index
+        p = message_params()
+        u = User.find(p[:user_id])
+        u.images.attach(p[:images])
+    end
+    
+    private
+        def message_params
+          params.require(:user_id).permit(images: [])
+        end
 end
